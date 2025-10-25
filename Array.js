ea9f893d3ecb82235cacc66af9c457bb47156eb7
panchal-krishn55 <panchalkrishn7@gmail.com>
2025-10-25 16:21:06 +0100

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,38 @@
+// let obj={
+//     name: 'krishn kumar panchal',
+//     surname:'bharatpur wale',
+//     weight: 50,
+//     height:'5 feet 3 inch',
+//     sub: function(){
+//         console.log("Hiii welcome to krish kumar panchal code studio");
+//     }
+
+
+// }
+// console.log(obj);
+// obj.sub();
+
+// let arr=[1,2,3,4,5,5,6,6,8]
+// console.log(arr);
+
+
+
+// let brr=new Array('true',1,true);
+// brr.push('NARENDRA');
+
+
+// brr.pop();
+// console.log(brr);
+
+
+
+// console.log(typeof(arr));
+// console.log(typeof(brr));
+let arr=[100,200,300];
+
+let answerArray=arr.map((Number)=>{
+    return number*number;
+
+})
+
+console.log(answerArray);
